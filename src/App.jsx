d2bef5ded2b8f08e1cadcb5f2f9bf2ee8e229c57
Caio <premiersoft-caio.zubek@ailos.coop.br>
2024-01-29 23:43:23 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,90 @@
 import { useState } from "react";
-import GameBoard from "./components/GameBoard"
-import Player from "./components/Player"
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
 import Log from "./components/Log";
-function App() {
+import { WINNING_COMBINATIONS } from "./assets/winning-combinations";
 
-  const [activePlayer, setActivePlayer] = useState('X');
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
+function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentPlayer) => currentPlayer === "X" ? "O" : "X");
-    setGameTurns(previousTurns => {
-      let player = 'X';
-      if (previousTurns.length > 0 && previousTurns[0].player === 'X') {
-        player = 'O';
-      }
-      const newTurns = [{ square: { row: rowIndex, col: colIndex }, player: player }, ...previousTurns];
+    setGameTurns((previousTurns) => {
+      const currentPlayer = deriveActivePlayer(previousTurns);
+      const newTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...previousTurns,
+      ];
       return newTurns;
     });
   }
 
+  let winner = undefined;
+
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
   return (
-    <main> <center ><h1>React Tic-Tac-Toe</h1> </center>
+    <main>
+      {" "}
+      <center>
+        <h1>React Tic-Tac-Toe</h1>{" "}
+      </center>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns} > </GameBoard>
+        {winner && <p> You Won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}>
+          {" "}
+        </GameBoard>
         <Log turns={gameTurns}></Log>
-      </div> </main>
-  )
+      </div>{" "}
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,11 @@
 export default function Log({ turns }) {
-    return <ol id='log' className="log-box"> {turns.map(turn => <h1> {turn.player} | {turn.square.row} / {turn.square.col}</h1>)} </ol>
+  return (
+    <ol id="log" className="log-box">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected | {turn.square.row} / {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 }
